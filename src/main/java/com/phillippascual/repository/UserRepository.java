c19f1f3bd257a116f50b7eb5d1a544cb33bfda22
Phillip Pascual <phillip.pascual@gmail.com>
2020-02-24 22:16:48 -0600

    Added documentation for the UserRepository Class.

--- src/main/java/com/phillippascual/repository/UserRepository.java
+++ src/main/java/com/phillippascual/repository/UserRepository.java
@@ -9,6 +9,10 @@ import org.apache.log4j.Logger;
 public class UserRepository {
 	private static Logger log = Logger.getLogger(UserRepository.class);
 	
+	/**
+	 * The insertNewUser() method takes in a String and a Connection.  It inserts the passed in username
+	 * String into the public.username table and returns the number of rows in the table that were updated.
+	 */
 	public static int insertNewUser(String username, Connection conn) {
 
 		String sqlStatement = "INSERT INTO public.usertable (username) VALUES (?)";
@@ -26,6 +30,10 @@ public class UserRepository {
 		return 0;
 	}
 	
+	/**
+	 * The removeUser() method takes in a String and a Connection.  It removes the public.usertable record that
+	 * matches the username that was passed in.
+	 */
 	public static int removeUser(String username, Connection conn) {
 		String sqlStatement = "DELETE from public.usertable WHERE username = ?";
 		try {
