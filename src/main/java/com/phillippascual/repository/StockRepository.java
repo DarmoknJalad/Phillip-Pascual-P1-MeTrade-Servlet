758872b9e99a60b02fe870e8e5bea4ccc9a2cb62
Phillip Pascual <phillip.pascual@gmail.com>
2020-02-24 22:10:18 -0600

    Added documentation to the StockRepository Class.

--- src/main/java/com/phillippascual/repository/StockRepository.java
+++ src/main/java/com/phillippascual/repository/StockRepository.java
@@ -14,9 +14,18 @@ import java.util.Scanner;
 
 import org.apache.log4j.Logger;
 
+/**
+ * The StockRepository Class contains all DDL and DML operations for the database used for this project.
+ */
 public class StockRepository {
 	private static Logger log = Logger.getLogger(StockRepository.class);
 	
+	/**
+	 * The addStock() method takes in 5 parameters: String username, String ticker, double price, int qty,
+	 * and Connection conn.  The method creates a Prepared SQL Statment that takes in the username, ticker, price,
+	 * and qty, and inserts a stock with the passed-in information into the public.stocks table.  This method first
+	 * requires that a user be added to the public.users table in order to satisfy one value in the SQL command.
+	 */
 	public static int addStock(String username, String ticker, double price, int qty, Connection conn) {
 		String sqlStatement = "INSERT INTO public.stocks (ticker, userid, price, qty) "
 				+ "VALUES (?, (SELECT userid FROM public.usertable WHERE username=?), "
@@ -38,6 +47,12 @@ public class StockRepository {
 		return 0;
 	}
 	
+	/**
+	 * The removeStock() method takes in 3 parameters: String username, String ticker, and Connection conn.
+	 * It generates a SQL statement that deletes 'ticker' from public.stocks for the username passed into the method.
+	 * It returns an in of the number of records updated, which should be 1 since there should be no duplicate records
+	 * in the public.stocks table.
+	 */
 	public static int removeStock(String username, String ticker, Connection conn) {
 		String sqlStatement = "DELETE FROM public.stocks WHERE userid=(SELECT userid FROM public.usertable WHERE "
 				+ "username=?) AND ticker=?";
@@ -55,6 +70,12 @@ public class StockRepository {
 		return 0;
 	}
 	
+	/**
+	 * The updateStockPrices() method takes in a Connection parameter.  It first retrieves a ResultSet of 
+	 * stock tickers from the public.stocks table.  It then iterates through the ResultSet and passes each ticker
+	 * into Mule to return a double of the stock price for each stock ticker.  It then updates the records with each
+	 * stock ticker in the public.stocks table.  The method then returns the total number of records updated.
+	 */
 	public static int updateStockPrices(Connection conn) {
 		String sqlStatement = "SELECT ticker FROM public.stocks";
 		String sqlStatement2 = "UPDATE public.stocks SET price = ?::double precision::numeric::money"
